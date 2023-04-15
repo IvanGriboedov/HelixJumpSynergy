d923b925afbf90c9afb5d672931a6e462191d0c8
AlexSulimov <sogeking.pro@gmail.com>
2023-04-15 20:43:15 +0300

    CreatedSwipeInput

---
+++ Assets/Scripts/Input/Swipe.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Input
+{
+    public readonly struct Swipe
+    {
+        public  Vector2 StartPos { get; }
+        public Vector2 EndPos { get; }
+        public Vector2 Delta { get; }
+        public Swipe(Vector2 startPos, Vector2 endPos, Vector2 delta)
+        {
+            StartPos = startPos;
+            EndPos = endPos;
+            Delta = delta;
+        }
+        public Vector2 NormalizeDelta => Delta.normalized;
+    }
+
+}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Input;
 public class Test : MonoBehaviour
 {
+    [SerializeField]private InputSwipePanel _swipePanel;
     // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
-        
-    } 
-    // Update is called once per frame
-    void Update()
+        _swipePanel.Swiping += OnSwiping;
+    }
+    private void OnDisable()
     {
-        
+        _swipePanel.Swiping -= OnSwiping;
+    }
+    private void OnSwiping(Swipe swipe)
+    {
+        transform.position += (Vector3)swipe.Delta;
     }
 }
