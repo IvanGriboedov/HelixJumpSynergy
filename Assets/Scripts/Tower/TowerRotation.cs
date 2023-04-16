1dc2f0760b52bb139fad0dd40474d0da71e90094
AlexSulimov <sogeking.pro@gmail.com>
2023-04-16 17:01:35 +0300

    Change Tower Rotation

--- Assets/Scripts/Tower/TowerRotation.cs
+++ Assets/Scripts/Tower/TowerRotation.cs
@@ -6,17 +6,20 @@ namespace Tower
     public class TowerRotation : MonoBehaviour
     {
         [SerializeField] [Min(0.0f)] private float _rotationSpeed;
-        [SerializeField] [Min(0.0f)] private float _angularDrag;
-        [SerializeField] private Rigidbody _rigidbody;
-        private void OnValidate()
-        {
-            _rigidbody.angularDrag = _angularDrag;
-        }
-        public void Rotate(float xAxis)
-        {
-            Vector3 torque = Vector3.up * xAxis * _rotationSpeed * Time.deltaTime * -1;
-            _rigidbody.AddTorque(torque, ForceMode.Acceleration);
 
+        private Quaternion _newRoationAngle;
+        private void Update()
+        {
+            transform.rotation = CalculateRotation(Time.deltaTime * _rotationSpeed);
+        }
+
+        private Quaternion CalculateRotation(float rotationSpeed) => 
+            Quaternion.Slerp(transform.rotation, _newRoationAngle, rotationSpeed);
+
+        public void AddRotation(float xAxis)
+        {
+            Vector3 newEulerRotationAngle = transform.eulerAngles + Vector3.down * xAxis;
+            _newRoationAngle = Quaternion.Euler(newEulerRotationAngle);
         }
     }
 }
