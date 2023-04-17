f8b2e5d58d0f194624ed7707b31294558eb1f79e
AlexSulimov <sogeking.pro@gmail.com>
2023-04-17 19:50:59 +0300

    Added ball effects

--- Assets/Scripts/Ball/BallParticles.cs
+++ Assets/Scripts/Ball/BallParticles.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 namespace Ball
 {
-    public class BallEffects : MonoBehaviour
+    public class BallParticles : MonoBehaviour
     {
+        private const float SurfaceYOffset = 0.06f;
+
         [SerializeField] private ParticleSystem _collisionParticlesPrefab;
+        [SerializeField] private ParticleSystem _spotParticlesPrefab;
+
         private ParticleSystem _collisionParticles;
         private void Start() =>
             _collisionParticles = Instantiate(_collisionParticlesPrefab);
 
-        private void OnCollisionEnter(Collision other) =>
+        private void OnCollisionEnter(Collision other)
+        {
             EmitCollisionParticles(other);
+            EmitSpotParticles(other);
+
+        }
 
         private void EmitCollisionParticles(Collision other)
         {
@@ -20,6 +28,11 @@ namespace Ball
 
             _collisionParticles.transform.position = collisionPosition;
             _collisionParticles.Play();
+        }
+        private void EmitSpotParticles(Collision other)
+        {
+            Vector3 collisionPosition = other.contacts[0].point + Vector3.up * SurfaceYOffset;
+            Instantiate(_spotParticlesPrefab, collisionPosition, Quaternion.identity, other.transform);
         }
     }
 
