da9264ee063292e263bfd01356d435d5d9e24c8f
AlexSulimov <sogeking.pro@gmail.com>
2023-04-17 17:56:43 +0300

    Created ball bounce

---
+++ Assets/Scripts/Physics/Bounce/Bounce.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Physics
+{
+    public class Bounce
+    {
+        private readonly Rigidbody _rigidbody;
+        private readonly BounceData _data;
+
+        public Bounce(Rigidbody rigidbody, BounceData bounceData)
+        {
+            _rigidbody = rigidbody;
+            _data = bounceData;
+        }
+
+
+        public void BounceOff(Vector3 direction) => 
+            _rigidbody.AddForce(direction * _data.Force, ForceMode.VelocityChange);
+
+        public void ClampHeight()
+        {
+            Vector3 velocity = _rigidbody.velocity;
+            _rigidbody.velocity = velocity.y >= 0.0f
+                ? Vector3.ClampMagnitude(velocity,_data.MaxHeight)
+                : velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/Structures/Vector3Curves.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Structures
+{
+    [System.Serializable]
+    public class Vector3Curves
+    {
+        [SerializeField] private AnimationCurve _xCurve;
+        [SerializeField] private AnimationCurve _yCurve;
+        [SerializeField] private AnimationCurve _zCurve;
+
+        public AnimationCurve XCurve { get => _xCurve; }
+        public AnimationCurve YCurve { get => _yCurve; }
+        public AnimationCurve ZCurve { get => _zCurve;  }
+    }
+}
