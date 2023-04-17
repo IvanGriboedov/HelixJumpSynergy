85b70d3ecb11b0570b9d04fe42da46f68eaf6cf9
AlexSulimov <sogeking.pro@gmail.com>
2023-04-17 22:38:01 +0300

    Created platform destroy scripts

--- Assets/Scripts/Platforms/Parts/PlatformPart.cs
+++ Assets/Scripts/Platforms/Parts/PlatformPart.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections;
+﻿using Physics.Ejection;
+using Extensions;
+using System.Collections;
 using UnityEngine;
 
 namespace Platforms.Parts
 {
     public abstract class PlatformPart : MonoBehaviour
     {
-
+        public void UnhookBy(EjectionSo ejection, Vector3 centerOfPlatform)
+        {
+            Rigidbody rigidbody = gameObject.AddComponent<Rigidbody>();
+            transform.ClearParent();
+            rigidbody.detectCollisions = false;
+            ejection.PushOut(rigidbody, centerOfPlatform);
+        }
     }
 }
---
+++ Assets/Scripts/Platforms/PlatformSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Platforms
+{
+    [CreateAssetMenu(fileName = "PlatformSettings", menuName = "ScriptableObjects/Platforms/Settings", order = 0)]
+    public class PlatformSettings : ScriptableObject
+    {
+        [SerializeField] [Min(0.0f)]private float _destroyDelayAfterUnhooking;
+
+        public float DestroyDelayAfterUnhooking { get => _destroyDelayAfterUnhooking; }
+    }
+}
